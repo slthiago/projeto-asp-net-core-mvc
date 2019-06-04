8371df02f355c4bdd6bf44a343b8f7cfea8cfe3f
Thiago Santos <thiagohillsong@hotmail.com>
2019-06-03 22:31:09 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,32 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SalesStatus Status { get; set; }
+        //coneta a classe SalesRecord à Seller
+        public Seller Seller { get; set; }
+        //construtor vazio
+        public SalesRecord()
+        {
+        }
+        //construtor com argumentos
+        public SalesRecord(int id, DateTime date, double amount, SalesStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+        
+
+
+
+    }
+}
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        //conecta a classe Seller à Department (muitos pra 1)
+        public Department Department { get; set; }
+        //conecta a classe Seller à Sales (1 pra muitos)
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+        //construtor vazio
+        public Seller()
+        {
+        }
+        //construtor com argumentos
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+        }
+        
+        //método para adicionar venda
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        //método para remover venda
+        public void RemoveSale(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+
+        }
+
+
+    }
+}
